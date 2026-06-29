33c030d9bb3e7a9e60f41f17bbd0b2ad13d7c9cf
Usman Ibrahim <techtalent247@gmail.com>
2026-06-29 11:02:25 +0100

    Fix: Add and configure correct Vite SWC plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc' // or '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc' // or '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
